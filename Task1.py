f772b4c2d6bee0c06f317747bcbd108f400ce1d0
karanzhere <35666568+karanzhere@users.noreply.github.com>
2025-04-10 15:14:59 +0530

    Add files via upload

---
+++ Task1.py
@@ -0,0 +1,17 @@
+'''
+#celcius from Celsius to Fahrenheit
+
+c=input("Celsius: ")
+c=float(c)
+
+f=(c*9/5) + 32
+print("fahrenheit: ",f)
+'''
+
+#Fahrenheit to celcius
+
+f=input("Fahrenheit: ")
+f=float(f)
+
+c=5*(f-32)/9
+print("Celsius: ",round(c,2),"°")
---
+++ Task2.py
@@ -0,0 +1,9 @@
+# Takes a user's first name and last name as input.
+Firstname = input("Enter your First Name: ")
+Lastname = input("Enter your Last Name: ")
+
+# Concatenates the first name and last name into a full name.
+Fullname = Firstname + " " + Lastname + "! "
+
+# Prints a personalized greeting message using the full name.
+print("Hello ",Fullname,"Welcome to the Python Program.")
